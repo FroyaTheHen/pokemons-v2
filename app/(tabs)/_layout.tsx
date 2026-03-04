12dbfbb884851a7a7f5b2261bc4e11b64d413ce3
FroyaTheHen <kingacepielik@gmail.com>
2026-03-04 12:11:19 +0100

    add assets, use in nav

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,47 @@
 import { Tabs } from 'expo-router';
+import { Image } from 'react-native';
+
+import homeFocused from '../../assets/icons/home-solid.png';
+import home from '../../assets/icons/home.png';
+import cameraFocused from '../../assets/icons/camera-solid.png';
+import camera from '../../assets/icons/camera.png';
+import mapFocused from '../../assets/icons/map-solid.png';
+import map from '../../assets/icons/map.png';
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="camera" options={{ title: 'Camera' }} />
-      <Tabs.Screen name="map" options={{ title: 'Map' }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#000000',
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ focused }) => (
+            <Image source={focused ? homeFocused : home} style={{ width: 24, height: 24 }} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: 'Camera',
+          tabBarIcon: ({ focused }) => (
+            <Image source={focused ? cameraFocused : camera} style={{ width: 24, height: 24 }} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: 'Map',
+          tabBarIcon: ({ focused }) => (
+            <Image source={focused ? mapFocused : map} style={{ width: 24, height: 24 }} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
